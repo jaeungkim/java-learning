cad702a247581ccda3dc2077ebc6a1b4f404d849
kiwoonjung <kiwoon0627@gmail.com>
2023-04-04 21:07:50 -0700

    first commit

--- src/Variables.java
+++ src/Variables.java
@@ -1,8 +1,24 @@
+import java.rmi.server.LogStream;
+
 public class Variables {
     public static void main(String[] args) {
-        int a;
-        a = 1;
+        char name = 'k';
+        char name2 = 'i';
+        String firstname = "kiwoon";
 
-        System.out.println(a+1); //2
+        System.out.println(name);
+        System.out.println(firstname);
+        System.out.println(firstname + name2);
+        System.out.println((firstname + name2) instanceof String);
+
+        int [] inArray = {1,2,3,4,5};
+        String [] stringArray = {"hello", "hello2"};
+
+        System.out.println(inArray[0]);
+        System.out.println(stringArray[0]);
+
+        for (int i = 0; i < inArray.length; i++) {
+            System.out.println(inArray[i]);
+        }
     }
 }
