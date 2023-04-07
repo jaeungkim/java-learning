2c0aabb2525fa95002b5c696dca9edddba19505b
Jae Kim <jkim@iclinicemr.com>
2023-04-06 17:14:13 -0700

    230406 05:13pm sudoku fun

---
+++ src/SudokuSolver.java
@@ -0,0 +1,111 @@
+public class SudokuSolver {
+    private int[][] board;
+
+    public SudokuSolver(int[][] board) {
+        this.board = board;
+    }
+
+    public boolean solveSudoku() {
+        int row = -1;
+        int col = -1;
+        boolean isEmpty = true;
+
+        // Find an empty cell
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+
+                    // We still have an empty cell
+                    isEmpty = false;
+                    break;
+                }
+            }
+
+            if (!isEmpty) {
+                break;
+            }
+        }
+
+        // If all cells are filled, the puzzle is solved
+        if (isEmpty) {
+            return true;
+        }
+
+        // Try values from 1 to 9 in the empty cell
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(row, col, num)) {
+                board[row][col] = num;
+
+                // Recursively try to solve the puzzle
+                if (solveSudoku()) {
+                    return true;
+                } else {
+                    // Backtrack and try a different number
+                    board[row][col] = 0;
+                }
+            }
+        }
+
+        // Puzzle cannot be solved
+        return false;
+    }
+
+    private boolean isValid(int row, int col, int num) {
+        // Check row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 box
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        // Number is valid
+        return true;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+        SudokuSolver solver = new SudokuSolver(board);
+        solver.solveSudoku();
+        solver.printBoard();
+    }
+}
--- src/Variables.java
+++ src/Variables.java
@@ -1,24 +1,12 @@
-import java.rmi.server.LogStream;
-
+import java.util.Scanner;
 public class Variables {
-    public static void main(String[] args) {
-        char name = 'k';
-        char name2 = 'i';
-        String firstname = "kiwoon";
+        public static void main(String[] args) {
 
-        System.out.println(name);
-        System.out.println(firstname);
-        System.out.println(firstname + name2);
-        System.out.println((firstname + name2) instanceof String);
+            int[] numbers = {1, 2, 3, 4, 5,6};
 
-        int [] inArray = {1,2,3,4,5};
-        String [] stringArray = {"hello", "hello2"};
-
-        System.out.println(inArray[0]);
-        System.out.println(stringArray[0]);
-
-        for (int i = 0; i < inArray.length; i++) {
-            System.out.println(inArray[i]);
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(numbers[i]);
+            }
         }
-    }
 }
+
